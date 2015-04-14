3c7e612919a45931e5f4f64eb59cfe010b874512
Benas Paulikas <Benaspggj@gmail.com>
2015-04-14 13:45:16 +0300

    Migrate database started

---
+++ migrations/m150414_102213_db.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150414_102213_db extends Migration
+{
+    public function up()
+    {
+		$this->createTable('student',[
+		
+		]);
+    }
+
+    public function down()
+    {
+        echo "m150414_102213_db cannot be reverted.\n";
+
+        return false;
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
